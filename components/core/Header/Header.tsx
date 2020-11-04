953c0edf4b1627425c8815bd4b3597a622cc48ae
Edgar López <edgar.lr.18@gmail.com>
2020-11-04 12:23:11 -0600

    Add header

--- components/core/Header/Header.tsx
+++ components/core/Header/Header.tsx
@@ -1,6 +1,28 @@
+import { useEffect, useState } from 'react'
 import s from './Header.module.css'
 const Header = () => {
-  return <header className={s.root}>Edgar López</header>
+  const [isShowed, setIsShowed] = useState(false)
+
+  const showText = () => {
+    if (window.scrollY > 320) {
+      setIsShowed(true)
+    } else {
+      setIsShowed(false)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', showText)
+    return () => {
+      window.removeEventListener('scroll', showText)
+    }
+  }, [])
+
+  return (
+    <header className={s.root}>
+      <div className={`${s.title} ${isShowed && s.show}`}>Edgar López</div>
+    </header>
+  )
 }
 
 export default Header
