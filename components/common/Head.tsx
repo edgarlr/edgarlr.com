e22b483b3a7fe4f5788b0582d282a60920d4b264
Edgar López <edgar.lr.18@gmail.com>
2021-03-08 19:46:15 -0600

    Update favicon

--- components/common/Head.tsx
+++ components/common/Head.tsx
@@ -60,54 +60,8 @@ const Head = ({ title, description, image }: Props) => {
 
       <link
         rel="apple-touch-icon"
-        sizes="57x57"
-        href="/favicons/apple-icon-57x57.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="60x60"
-        href="/favicons/apple-icon-60x60.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="72x72"
-        href="/favicons/apple-icon-72x72.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="76x76"
-        href="/favicons/apple-icon-76x76.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="114x114"
-        href="/favicons/apple-icon-114x114.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="120x120"
-        href="/favicons/apple-icon-120x120.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="144x144"
-        href="/favicons/apple-icon-144x144.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="152x152"
-        href="/favicons/apple-icon-152x152.png"
-      />
-      <link
-        rel="apple-touch-icon"
         sizes="180x180"
-        href="/favicons/apple-icon-180x180.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="192x192"
-        href="/favicons/android-icon-192x192.png"
+        href="/favicons/apple-touch-icon.png"
       />
       <link
         rel="icon"
@@ -118,18 +72,22 @@ const Head = ({ title, description, image }: Props) => {
       <link
         rel="icon"
         type="image/png"
-        sizes="96x96"
-        href="/favicons/favicon-96x96.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
         sizes="16x16"
         href="/favicons/favicon-16x16.png"
       />
-      <link rel="manifest" href="/favicons/manifest.json" />
+      <link rel="manifest" href="/favicons/site.webmanifest" />
+      <link
+        rel="mask-icon"
+        href="/favicons/safari-pinned-tab.svg"
+        color="#131414"
+      />
+      <meta name="msapplication-TileColor" content="#131414" />
+      <meta name="theme-color" content="#ffffff" />
       <meta name="msapplication-TileColor" content="#000" />
-      <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+      <meta
+        name="msapplication-TileImage"
+        content="/favicons/ms-icon-144x144.png"
+      />
       <meta name="theme-color" content="#000" />
     </NextHead>
   )
