fada7badd862321d44e82649c17e4903b2ab37e4
Edgar López <edgar.lr.18@gmail.com>
2021-03-13 00:21:07 -0600

    Add author to SEO

--- components/common/Head.tsx
+++ components/common/Head.tsx
@@ -55,8 +55,6 @@ const Head = ({ title, description, image, type, date }: Props) => {
       <meta property="og:url" content={`https://edgarlr.com${router.asPath}`} />
       <meta property="og:image" content={image} />
 
-      <meta name="author" content="Edgar López" />
-
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
 
@@ -65,7 +63,7 @@ const Head = ({ title, description, image, type, date }: Props) => {
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:image" content={image} />
       <meta name="twitter:site" content="@edgarlr_" />
-
+      <meta property="article:author" content="https://twitter.com/edgarlr_" />
       {date && <meta property="article:published_time" content={date} />}
 
       <link
