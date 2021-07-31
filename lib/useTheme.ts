c674eaa8d0b0def446689015ecedf4ab67d96a2d
Edgar López <edgar.lr.18@gmail.com>
2021-07-31 12:17:05 -0500

    Add useIsomorphicLayoutEffect

--- lib/useTheme.ts
+++ lib/useTheme.ts
@@ -1,12 +1,13 @@
-import { useLayoutEffect, useState } from 'react'
+import { useState } from 'react'
 import { useLocalStorage } from './useLocalStorage'
+import { useIsomorphicLayoutEffect } from '@lib/hooks/use-isomorphic-layout-effect'
 
 export const useTheme = () => {
   const { storedValue, setLocalStorage } = useLocalStorage('theme', 'auto')
 
   const [theme, setTheme] = useState(storedValue)
 
-  useLayoutEffect(() => {
+  useIsomorphicLayoutEffect(() => {
     setLocalStorage(JSON.stringify(theme))
     document.documentElement.dataset.theme = theme
   }, [theme])
