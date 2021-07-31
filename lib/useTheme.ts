288ab9c103c56990b7bce97186be662b6f52bd75
Edgar López <edgar.lr.18@gmail.com>
2021-07-31 15:27:48 -0500

    Rever useIsomorphic

--- lib/useTheme.ts
+++ lib/useTheme.ts
@@ -1,13 +1,12 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useLocalStorage } from './useLocalStorage'
-import { useIsomorphicLayoutEffect } from '@lib/hooks/use-isomorphic-layout-effect'
 
 export const useTheme = () => {
   const { storedValue, setLocalStorage } = useLocalStorage('theme', 'auto')
 
   const [theme, setTheme] = useState(storedValue)
 
-  useIsomorphicLayoutEffect(() => {
+  useEffect(() => {
     setLocalStorage(JSON.stringify(theme))
     document.documentElement.dataset.theme = theme
   }, [theme])
