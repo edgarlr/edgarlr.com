e082043f91f22d2239bc5ab0463a5b0477beeb70
Edgar López <edgar.lr.18@gmail.com>
2021-07-31 10:25:27 -0500

    use useLayoutEffect in useTheme

--- lib/useTheme.ts
+++ lib/useTheme.ts
@@ -1,11 +1,12 @@
-import { useEffect, useState } from 'react'
+import { useLayoutEffect, useState } from 'react'
 import { useLocalStorage } from './useLocalStorage'
 
 export const useTheme = () => {
   const { storedValue, setLocalStorage } = useLocalStorage('theme', 'auto')
+
   const [theme, setTheme] = useState(storedValue)
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     setLocalStorage(JSON.stringify(theme))
     document.documentElement.dataset.theme = theme
   }, [theme])
