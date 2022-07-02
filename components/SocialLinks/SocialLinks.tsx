7217de71718cb60e3a05ccace044dc010f5fe8d2
Edgar Lopez <edgar.lr.18@gmail.com>
2022-07-02 12:13:06 -0500

    Update Social Links

--- components/SocialLinks/SocialLinks.tsx
+++ components/SocialLinks/SocialLinks.tsx
@@ -1,36 +1,35 @@
-import Dribbble from '@components/icons/Dribbble'
-import Figma from '@components/icons/Figma'
-import Github from '@components/icons/Github'
-import Linkedin from '@components/icons/Linkedin'
-import Twitter from '@components/icons/Twitter'
-import { IconLink } from '@components/ui/IconLink'
-import {
-  FigmaURL,
-  GithubURL,
-  TwitterURL,
-  LinkedinURL,
-  DribbbleURL,
-} from '@lib/constants'
+import { ButtonList, ButtonTrigger } from '@components/ui/button'
+import { FigmaURL, GithubURL, TwitterURL, LinkedinURL } from '@lib/constants'
+import { ElementType } from 'react'
 import s from './SocialLinks.module.css'
 
 const SocialLinks = () => {
+  const getButtonTriggerProps = (href: string) => ({
+    as: 'a' as ElementType<any>,
+    target: '_blank',
+    rel: 'noreferrer noopener',
+    id: href,
+    href,
+    className: s.button,
+    withIcon: true,
+  })
+
   return (
     <div className={s.root}>
-      <IconLink href={TwitterURL} ariaLabel="Link to twitter">
-        <Twitter width="20" height="20" />
-      </IconLink>
-      <IconLink href={GithubURL} ariaLabel="Link to gihub">
-        <Github width="20" height="20" />
-      </IconLink>
-      <IconLink href={FigmaURL} ariaLabel="Link to figma">
-        <Figma width="20" height="20" />
-      </IconLink>
-      <IconLink href={LinkedinURL} ariaLabel="Link to Linkedin">
-        <Linkedin width="20" height="20" />
-      </IconLink>
-      <IconLink href={DribbbleURL} ariaLabel="Link to dribble">
-        <Dribbble width="18" height="18" />
-      </IconLink>
+      <ButtonList>
+        <ButtonTrigger {...getButtonTriggerProps(TwitterURL)}>
+          Twitter
+        </ButtonTrigger>
+        <ButtonTrigger {...getButtonTriggerProps(GithubURL)}>
+          Github
+        </ButtonTrigger>
+        <ButtonTrigger {...getButtonTriggerProps(FigmaURL)}>
+          Figma
+        </ButtonTrigger>
+        <ButtonTrigger {...getButtonTriggerProps(LinkedinURL)}>
+          LinkedIn
+        </ButtonTrigger>
+      </ButtonList>
     </div>
   )
 }
