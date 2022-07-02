fce67ffb5a53a6afe5302ffde909060f9439ef1d
Edgar Lopez <edgar.lr.18@gmail.com>
2022-07-02 12:12:53 -0500

    Update Project Card

--- components/project/ProjectCard.tsx
+++ components/project/ProjectCard.tsx
@@ -1,6 +1,3 @@
-import Figma from '@components/icons/Figma'
-import Github from '@components/icons/Github'
-import { IconLink } from '@components/ui/IconLink'
 import s from './Project.module.css'
 import { ArrowUpRight } from '@components/icons/ArrowUpRight'
 import { ButtonList } from '@components/ui/button/ButtonList'
@@ -14,49 +11,38 @@ type Props = {
   links: TProjectLink[]
 }
 
-const Project = (props: Props) => {
-  const { title, href, description, info, links } = props
-
-  const projectLinkIcons = {
-    Figma,
-    Github,
-  }
-
-  return (
-    <div className={s.root}>
-      <div className={s.projectTitle}>
-        <a href={href} target="_blank" rel="noreferrer">
-          <h2>
-            {title}
-            <span className={s.icon}>
-              <ArrowUpRight width={18} height={18} />
-            </span>
-          </h2>
-        </a>
-      </div>
-      <div className={s.projectBody}>
-        <p>{description}</p>
-        <span className={s.info}>{info}</span>
-        <ButtonList className={s.links}>
-          {links.map(({ icon, url, title }: TProjectLink) => {
-            const IconComp = projectLinkIcons[icon]
-            return (
-              <ButtonTrigger key={url} id={url}>
-                <IconLink
-                  href={url}
-                  ariaLabel={`Link to ${title}`}
-                  title={title}
-                  label={title}
-                >
-                  <IconComp width={14} height={14} />
-                </IconLink>
-              </ButtonTrigger>
-            )
-          })}
-        </ButtonList>
-      </div>
+const Project = ({ title, href, description, info, links }: Props) => (
+  <div className={s.root}>
+    <div className={s.projectTitle}>
+      <a href={href} target="_blank" className={s.projectLink} rel="noreferrer">
+        <h2>
+          {title}
+          <span className={s.icon}>
+            <ArrowUpRight width={18} height={18} />
+          </span>
+        </h2>
+      </a>
     </div>
-  )
-}
+    <div className={s.projectBody}>
+      <p className={s.description}>{description}</p>
+      <span className={s.info}>{info}</span>
+      <ButtonList className={s.links}>
+        {links.map(({ url, title }: TProjectLink) => (
+          <ButtonTrigger
+            key={url}
+            id={url}
+            as="a"
+            href={url}
+            target="__black"
+            rel="noreferrer noopener"
+            withIcon
+          >
+            {title}
+          </ButtonTrigger>
+        ))}
+      </ButtonList>
+    </div>
+  </div>
+)
 
 export default Project
