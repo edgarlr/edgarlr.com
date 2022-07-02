810018d051bf717cb8457fbd8180fac034039a97
Edgar Lopez <edgar.lr.18@gmail.com>
2022-07-02 12:13:40 -0500

    Update IconLink styles

--- components/ui/IconLink/IconLink.tsx
+++ components/ui/IconLink/IconLink.tsx
@@ -4,23 +4,21 @@ import s from './IconLink.module.css'
 
 type Props = AnchorHTMLAttributes<HTMLAnchorElement> & {
   children: React.ReactNode
-  label?: string
   href: string
   ariaLabel: string
 }
 
-const IconLink = ({ children, label, href, ariaLabel, ...rest }: Props) => {
+const IconLink = ({ children, href, ariaLabel, ...rest }: Props) => {
   return (
     <a
       className={s.root}
       href={href}
       target="_blank"
-      rel="noreferrer"
+      rel="noreferrer noopener"
       aria-label={ariaLabel}
       {...rest}
     >
       {children}
-      {label && <p className={s.label}>{label}</p>}
       <span className={s.arrowIcon}>
         <ArrowUpRight width={14} height={14} />
       </span>
