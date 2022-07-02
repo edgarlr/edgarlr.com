a2611d300a815f06046892c002c6477da88202a0
Edgar Lopez <edgar.lr.18@gmail.com>
2022-07-02 12:13:28 -0500

    Update Button

--- components/ui/button/ButtonTrigger.tsx
+++ components/ui/button/ButtonTrigger.tsx
@@ -1,23 +1,47 @@
-import { ButtonHTMLAttributes, DetailedHTMLProps, FC } from 'react'
 import { useButtonListHover } from './use-button-hover'
 import cn from 'classnames'
 import s from './Button.module.css'
+import { ArrowUpRight } from '@components/icons/ArrowUpRight'
 
-type ButtonTriggerProps = DetailedHTMLProps<
-  ButtonHTMLAttributes<HTMLButtonElement>,
-  HTMLButtonElement
+type AsProp<C extends React.ElementType> = {
+  as?: C
+}
+
+type PropsToOmit<C extends React.ElementType, P> = keyof (AsProp<C> & P)
+
+type PolymorphicComponentProp<
+  C extends React.ElementType,
+  Props = Record<string, never>
+> = React.PropsWithChildren<Props & AsProp<C>> &
+  Omit<React.ComponentPropsWithoutRef<C>, PropsToOmit<C, Props>>
+
+type ButtonProps<C extends React.ElementType> = PolymorphicComponentProp<
+  C,
+  { className?: string; withIcon?: boolean }
 >
-
-export const ButtonTrigger: FC<ButtonTriggerProps> = ({
+export const ButtonTrigger = <C extends React.ElementType = 'button'>({
   className = '',
+  as,
+  children,
+  withIcon,
   ...props
-}) => {
+}: ButtonProps<C>) => {
   const { repositionHover } = useButtonListHover()
+
+  const Comp = as ?? 'button'
+
   return (
-    <button
+    <Comp
       {...props}
       onMouseEnter={repositionHover}
       className={cn(s.buttonTrigger, className)}
-    />
+    >
+      {children}
+      {withIcon && (
+        <span className={s.arrowIcon}>
+          <ArrowUpRight width={14} height={14} />
+        </span>
+      )}
+    </Comp>
   )
 }
