ed0eabecaef35cd78395d18fb3697b83931e16d9
Edgar Lopez <edgar.lr.18@gmail.com>
2022-07-03 11:56:40 -0500

    Update Article List component

--- components/articles/ArticlesList.tsx
+++ components/articles/ArticlesList.tsx
@@ -1,7 +1,7 @@
 import ArrowRight from '@components/icons/ArrowRight'
 import dateFormatter from '@lib/dateFormatter'
 import Link from 'next/link'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import s from './ArticlesList.module.css'
 
 type Props = {
@@ -10,16 +10,39 @@ type Props = {
 }
 
 const ArticlesList = ({ posts, paginate }: Props) => {
-  const PAGE_MAX = 5
-  const [loadedArticles, setLoadedArticles] = useState(PAGE_MAX)
+  const PAGE_LIMIT = 3
+  const [loadedArticles, setLoadedArticles] = useState(PAGE_LIMIT)
+
+  const entryRefs = useRef<Array<HTMLAnchorElement | null>>([])
+
+  const getRootHeight = () => {
+    const DEFAULT_ENTRY_HEIGHT = 78
+    if (!entryRefs.current || entryRefs.current.length === 0)
+      return DEFAULT_ENTRY_HEIGHT * 3
+
+    const heights = entryRefs.current.map(
+      (entry) => entry?.getBoundingClientRect().height ?? DEFAULT_ENTRY_HEIGHT
+    )
+
+    return heights.slice(0, loadedArticles).reduce((acc, curr) => acc + curr, 0)
+  }
 
   return (
-    <div className={s.root}>
-      {posts
-        .slice(0, paginate ? loadedArticles : undefined)
-        .map(({ slug, date, title }) => (
+    <>
+      <div
+        className={s.root}
+        style={
+          { '--rootHeight': `${getRootHeight()}px` } as React.CSSProperties
+        }
+      >
+        {posts.map(({ slug, date, title }, i) => (
           <Link href={`/articles/${slug}`} key={slug}>
-            <a className={`${s.entry} no-user-select`}>
+            <a
+              className={`${s.entry} no-user-select`}
+              ref={(el) => {
+                entryRefs.current[i] = el
+              }}
+            >
               <p className={` ${s.date}`}>{dateFormatter(date)}</p>
               <p className={s.title}>
                 {title}{' '}
@@ -30,16 +53,18 @@ const ArticlesList = ({ posts, paginate }: Props) => {
             </a>
           </Link>
         ))}
-
+      </div>
       {paginate && loadedArticles < posts.length && (
-        <button
-          className={s.button}
-          onClick={() => setLoadedArticles(loadedArticles + PAGE_MAX)}
-        >
-          Load more
-        </button>
+        <div className={s.buttonWrapper}>
+          <button
+            className={s.button}
+            onClick={() => setLoadedArticles(loadedArticles + PAGE_LIMIT)}
+          >
+            Load more
+          </button>
+        </div>
       )}
-    </div>
+    </>
   )
 }
 
