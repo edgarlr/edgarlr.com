0875469316b02d34486629634aef9d599f061ca3
Edgar Lopez <edgar.lr.18@gmail.com>
2024-02-24 20:51:54 +0100

    remove unused code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -29,16 +29,4 @@ function MyApp({ Component, pageProps }: AppProps) {
   )
 }
 
-// Only uncomment this method if you have blocking data requirements for
-// every single page in your application. This disables the ability to
-// perform automatic static optimization, causing every page in your app to
-// be server-side rendered.
-//
-// MyApp.getInitialProps = async (appContext: AppContext) => {
-//   // calls page's `getInitialProps` and fills `appProps.pageProps`
-//   const appProps = await App.getInitialProps(appContext);
-
-//   return { ...appProps }
-// }
-
 export default MyApp
