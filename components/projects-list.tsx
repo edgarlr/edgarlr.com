f32e0f48db0480b35fb1de2828438dd9c057bbe5
Edgar Lopez <edgar.lr.18@gmail.com>
2024-02-28 11:58:29 +0100

    add some projects

--- components/projects-list.tsx
+++ components/projects-list.tsx
@@ -12,18 +12,50 @@ export const ProjectsList = () => {
         src="/assets/projects/planetscale-homepage.png"
       />
       <ProjectLink
-        date="10/10/2023"
+        date="03/01/2023" // March 2023
         title="Landing page"
         company="Lapsso"
         href="https://www.laps.so"
         src="/assets/projects/lapsso-landing.png"
       />
       <ProjectLink
-        date="10/10/2023"
+        date="04/01/2023" // April 2023
         title="Remote pay strategy guide"
         company="Plane"
         href="https://plane.com/compensation-guide"
         src="/assets/projects/plane-compensation-guide.png"
+      />
+
+      <ProjectLink
+        date="05/1/2022" // May 2022
+        title="Business page"
+        company="InDebted"
+        href="https://indebted.co/business"
+        src="/assets/projects/indebted-business.png"
+      />
+
+      <ProjectLink
+        date="12/1/2021" // December 2021
+        title="Welcome to intercom"
+        company="Intercom"
+        href="https://intercom.com"
+        src="/assets/projects/intercom-welcome-to-intercom.png"
+      />
+
+      <ProjectLink
+        date="09/1/2021" // Septermber 2021
+        title="Conversational support page"
+        company="Intercom"
+        href="https://intercom.com/customer-support-software"
+        src="/assets/projects/intercom-conversational-support.png"
+      />
+
+      <ProjectLink
+        date="08/01/2021" // August 2021
+        title="Pricing page"
+        company="Intercom"
+        href="https://intercom.com/pricing"
+        src="/assets/projects/intercom-pricing.jpg"
       />
     </ul>
   )
