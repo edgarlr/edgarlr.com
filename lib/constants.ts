208d81381f543a3f9a1bfd532abf29a27d3ef75f
Edgar Lopez <edgar.lr.18@gmail.com>
2024-03-01 16:50:32 +0100

    remove unused

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,7 +1,5 @@
 // Meta Links
 export const SiteURL = 'https://www.edgarlr.com'
-export const SocialCard =
-  'https://res.cloudinary.com/dliiwavlg/image/upload/v1656947753/edgarlr_og_i0mzqy.jpg'
 
 // Social Links
 export const TwitterURL = 'https://twitter.com/edgarlr_'
@@ -11,4 +9,3 @@ export const FigmaURL = 'https://www.figma.com/@edgarlr'
 export const DribbbleURL = 'https://dribbble.com/edgarlr'
 export const LinkedinURL = 'https://www.linkedin.com/in/edgarlr/'
 
-export const RepoUrl = 'https://github.com/edgarlr/portfolio'
