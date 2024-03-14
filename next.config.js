63ee01e7e88b018bb9617bc83cf8ad99f19090e4
Edgar Lopez <edgar.lr.18@gmail.com>
2024-03-14 12:36:53 +0100

    rename post

--- next.config.js
+++ next.config.js
@@ -6,6 +6,11 @@ module.exports = {
   async redirects() {
     return [
       {
+        source: '/posts/react-automatic-slider-pure-css',
+        destination: '/posts/infinite-marquee-animation',
+        permanent: true,
+      },
+      {
         source: '/articles/:path*',
         destination: '/posts/:path*',
         permanent: true,
