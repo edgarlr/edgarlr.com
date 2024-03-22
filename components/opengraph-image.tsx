39d1b29f7e77aa8f0e96cde214419a3b3815dbee
Edgar Lopez <edgar.lr.18@gmail.com>
2024-03-22 15:02:57 +0100

    add syntaxt highlight

---
+++ components/opengraph-image.tsx
@@ -0,0 +1,49 @@
+import { ImageResponse } from 'next/og';
+import React from 'react'
+
+const baseUrl = process.env.VERCEL_BRANCH_URL ? `https://${process.env.VERCEL_BRANCH_URL}` : 'http://localhost:3000'
+
+export const OpengraphImage = async ({ date: dateProp, title }: { title?: string, date?: string }) => {
+  const date = new Date(dateProp ?? '').toLocaleDateString('default', {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+  })
+
+  const interRegular = fetch(new URL('/fonts/Inter-Regular.ttf', baseUrl)).then((res) =>
+    res.arrayBuffer()
+  )
+
+
+  const newsreaderItalic = fetch(new URL('/fonts/Newsreader-Italic.ttf', baseUrl)).then((res) =>
+    res.arrayBuffer()
+  )
+
+  return new ImageResponse(
+    (
+      <div style={{ background: 'radial-gradient(100% 100% at 50% 0%,#FFF,#FAFAFA)' }} tw="flex h-full w-full text-[#131414] flex-col px-48 justify-center">
+        {<div style={{ fontFamily: 'Newsreader' }} tw="mb-4 italic text-5xl">{title}</div>}
+        {date && <div tw="opacity-60 text-2xl">{date}</div>}
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630,
+      fonts: [
+        {
+          name: 'Inter',
+          data: await interRegular,
+          style: 'normal',
+          weight: 400,
+        },
+        {
+          name: 'Newsreader',
+          data: await newsreaderItalic,
+          style: 'italic',
+          weight: 400,
+        },
+      ],
+    }
+  );
+
+}
