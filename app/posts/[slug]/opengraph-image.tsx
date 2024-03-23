0efc314620237ba96cda0d05778b1328ac96f29e
Edgar Lopez <edgar.lr.18@gmail.com>
2024-03-23 23:28:02 +0100

    test without mdx

--- app/posts/[slug]/opengraph-image.tsx
+++ app/posts/[slug]/opengraph-image.tsx
@@ -1,10 +1,10 @@
 import { OpengraphImage } from '@components/opengraph-image';
-import { getPostMetadataBySlug } from '@lib/posts';
+// import { getPostMetadataBySlug } from '@lib/posts';
 
 export const runtime = 'nodejs';
 
 export default async function Image({ params }: { params: { slug: string } }) {
-  const post = await getPostMetadataBySlug(params.slug)
+  // const post = await getPostMetadataBySlug(params.slug)
 
-  return await OpengraphImage({ title: post?.title, date: post.date })
+  return await OpengraphImage({ title: "post?.title", date: "02/02/2020" })
 }
